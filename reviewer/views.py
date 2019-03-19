5f1cecd1c989f1dc4b3eeea2b0024f2ac4d2190c
rxdc <mjm29856@gmail.com>
2019-03-19 19:40:57 -0400

    added support for remembering session history via cookies

--- reviewer/views.py
+++ reviewer/views.py
@@ -3,19 +3,26 @@ from django.http import Http404
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
-from .models import Professor, ReviewSnapshot, Review
+from .models import Professor, ReviewSnapshot, Review, Session
 from RMPScraper import getRMPReviews, ProfessorSearch
 from django.utils import timezone
+
+def getProfessorHistory(session_id):
+    session_obj = Session.objects.get(id=session_id)
+    prof_list = session_obj.history.all()
+    return prof_list
 
 def index(request):
     popular_professors = sorted(Professor.objects.all(), key=lambda professor: professor.hitCounter, reverse=True)[:5]
     recent_professors = sorted(Professor.objects.all(), key=lambda professor: professor.lastUpdated, reverse=True)[:5]
     total_professors = len(Professor.objects.all())
     total_reviews = len(Review.objects.all())
-    context = {'recent_professors': recent_professors, 'popular_professors': popular_professors, 'total_professors': total_professors, 'total_reviews': total_reviews}
+    prof_history = getProfessorHistory(id)
+    context = {'prof_history': prof_history, 'recent_professors': recent_professors, 'popular_professors': popular_professors, 'total_professors': total_professors, 'total_reviews': total_reviews}
     return render(request, "reviewer/index.html", context)
 
 def professor(request, id):
+    prof_history = getProfessorHistory(id)
     try:
         professor = Professor.objects.get(id=id)
         professor.hitCounter = professor.hitCounter + 1
@@ -32,7 +39,16 @@ def professor(request, id):
         professor.save()
     except Professor.DoesNotExist:
         raise Http404("Professor does not exist.")
-    return render(request, 'reviewer/professor.html', {'professor': professor})
+    response = render(request, 'reviewer/professor.html', {'prof_history': prof_history, 'professor': professor})
+    try:
+        session_id = request.COOKIES["session_id"]
+        my_session = getProfessorHistory(session_id)
+        my_session.history.add(id)
+    except:
+        new_session = Session()
+        new_session.save()
+        response.set_cookie(key="session_id", value=new_session.id)
+    return response
 
 def results(request, name):
     professors = ProfessorSearch(name)
