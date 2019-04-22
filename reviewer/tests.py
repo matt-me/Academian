b0e089457ad09a0ccffdbdad3aef4876be0d0604
rxdc <mjm29856@gmail.com>
2019-04-22 15:35:26 -0400

    added first test

--- reviewer/tests.py
+++ reviewer/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Professor
+from django.utils import timezone
 
 # Create your tests here.
+class dopplegangerTests(TestCase):
+    def getsdopplegangerTests(self):
+        professor1 = Professor(name="Michael Read", school="UVA", department="Computer Science", lastUpdated=timezone.now(), hitCounter=1)
+        professor1.save()
+        professor2 = Professor(name="Michael Read", school="UVA", department="Computer Science", lastUpdated=timezone.now(), hitCounter=1)
+        professor2.save()
+        self.assertTrue(professor2 in professor1.getDopplegangers())
+       
