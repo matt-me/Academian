347382c36b075c211eb10c28c9bf44d5eb5c6857
rxdc <mjm29856@gmail.com>
2019-05-20 22:44:27 -0400

    more work on the reddit scraper

--- RedditScraper.py
+++ RedditScraper.py
@@ -1,7 +1,21 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import datetime
-document = urllib.request.urlopen(url).read()
+import json
+# search for a subreddit using a name, typically this would be used to find the school
 def SubredditSearch(query):
     query = str(query).replace(" ", "+")
-    url = "https://www.reddit.com/subreddits/search?q=query"
+    url = "https://www.reddit.com/subreddits/search?q=" + query
+    document = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(document, "html.parser")
+    results = soup.findAll(name="a", attrs={"title"}, limit=1)
+    link = None
+    if len(results) == 1:
+        link = results[0].attrs["href"]
+    return link
+
+def GetThread(url):
+    json1 = urllib.request.urlopen(url).read()
+    json2 = json.loads(json1)
+    pass
+GetThread("https://www.reddit.com/r/gmu/search.json?q=David+Nordstrom&restrict_sr=true")
