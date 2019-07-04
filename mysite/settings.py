02f57059e39a4a6ed1ff3ad808fb353d0c433652
rxdc <mjm29856@gmail.com>
2019-07-04 15:08:29 -0400

    hid the django secret key which is now in a file called secret_settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wlgvwk-hcr7rdfn89k78++ft98@@+k3oq6ekt=_t!u+@s5^m*@'
+from mysite.secret_settings import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mod_wsgi.server'
+    #'mod_wsgi.server'
 ]
 
 MIDDLEWARE = [
