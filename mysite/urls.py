83d0f15e4ad13b4a94b1b71c22f45aaaf24cca02
rxdc <mjm29856@gmail.com>
2019-12-05 20:07:38 -0500

    removed the "academician" part of the url

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('academian/', include('reviewer.urls')),
+    path('', include('reviewer.urls')),
 ]
